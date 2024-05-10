7040c1ff7059ba58fe8daf85a2aef6ddddf15c86
Vamkrish2812 <166794191+Vamkrish2812@users.noreply.github.com>
2024-05-10 15:15:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+#this is a simple flask Application that prints hello world!
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello():
+    return "<h2>Hello world -Docker Flask Deployment Lab</h2><hr/>"
+
+app.run(host="0.0.0.0",port=5000)
